57a517123b7c8c54b99181f86d876af4bdfc06dc
snowd <snowmaos@gmail.com>
2020-01-30 11:58:53 +0800

    Add router guard for login

--- PunchSystem/src/app/app-routing.module.ts
+++ PunchSystem/src/app/app-routing.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {MemberprofileComponent} from './memberprofile/memberprofile.component';
-import {LoginComponent} from './login/login.component';
-import {LeaveComponent} from './leave/leave.component';
-import {PunchinComponent} from './punchin/punchin.component';
-const routes: Routes =  [
+import { MemberprofileComponent } from './memberprofile/memberprofile.component';
+import { LoginComponent } from './login/login.component';
+import { LeaveComponent } from './leave/leave.component';
+import { PunchinComponent } from './punchin/punchin.component';
+import { AuthGuard } from '../app/auth/auth.guard';
+const routes: Routes = [
 
   // /:id = 必須帶參數才能過去
-  {path: '' , redirectTo: 'login' , pathMatch: 'full' },
-  {path: 'login' , component: LoginComponent},
-  {path: 'member' , component: MemberprofileComponent},
-  {path:'leave' , component: LeaveComponent},
-  {path:'punch' , component: PunchinComponent}
-  // {path: '**' , component: ContetnComponent},
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'member', component: MemberprofileComponent, canActivate: [AuthGuard] },
+  { path: 'leave', component: LeaveComponent, canActivate: [AuthGuard] },
+  { path: 'punch', component: PunchinComponent, canActivate: [AuthGuard] }
   // {path: '**' , redirectTo: 'home' , pathMatch: 'full' },
   // fallbackRoute
 ];
